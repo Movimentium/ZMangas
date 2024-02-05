d8a2124e2d873551ef7b5cccbf8aa9255e675ddc
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2024-02-05 20:31:35 +0100

    Initial Commit

---
+++ ZMangas/ZMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZMangasApp.swift
+//  ZMangas
+//
+//  Created by Miguel Gallego on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ZMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
